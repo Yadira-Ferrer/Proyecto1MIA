3b03263ced1b11f5626bce84ee8355b6bbdccd0a
yadira-ferrer <yyfm.95@gmail.com>
2020-08-12 00:35:47 -0600

    Lectura de los comandos.

--- Proyecto/main.go
+++ Proyecto/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	var comando string = ""
+	entrada := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Printf("[Ingrese Comando]: ")
+		entrada.Scan()
+		comando = entrada.Text()
+		if strings.ToLower(comando) == "salir" {
+			break
+		}
+		analizar(comando)
+	}
 }
--- Proyecto/scanner.go
+++ Proyecto/scanner.go
@@ -1,1 +1,11 @@
 package main
+
+import "fmt"
+
+func analizar(entrada string) {
+	fmt.Println("Analizando entrada...")
+	for _, c := range entrada {
+		l := string(c)
+		fmt.Println(l)
+	}
+}
